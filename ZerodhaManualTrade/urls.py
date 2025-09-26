c452d640cfd080eb2efadd4689490b067b00ca6c
Prateek Kumar Singh <prateksingh4435@gmail.com>
2025-09-26 14:58:51 +0530

    add

--- ZerodhaManualTrade/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import  *
-
-urlpatterns = [ 
-            path('getzerodhatoken/',GetZerodhaTradingSymbol.as_view()),
-            path('buyorderapi/',ZerodhaBuyOrderAPIView.as_view()),
-            path('sellorderapi/',ZerodhaSellOrderAPIView.as_view()),
-           
-]
