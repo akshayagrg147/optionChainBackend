6b8aeecb54ebd711f075eebfc994cf02713cecd9
Prateek Kumar Singh <prateksingh4435@gmail.com>
2025-09-29 13:55:26 +0530

    adddata

--- copy_trading/consumers.py
+++ copy_trading/consumers.py
@@ -378,7 +378,9 @@ class LiveOptionDataConsumer(AsyncWebsocketConsumer):
                                             if order_response.get("status") == "success":                                                
                                                 order_id = order_response["data"]["order_ids"][0]
                                                 print('order_id',order_id)
-                                                detail_data = fetch_order_status(order_id, access_token)
+                                                detail_data = await asyncio.get_event_loop().run_in_executor(
+                                                                        None, fetch_order_status, order_id, access_token
+     )
                                                 if detail_data and detail_data.get("status") == "success":
                                                     order_status = detail_data["data"]["status"]
                                                     if order_status == "complete":
