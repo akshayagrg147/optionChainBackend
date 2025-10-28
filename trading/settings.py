3a6a0d31ae75b3d20df2cfb0d5e2f730c256faa9
Prateek Kumar Singh <prateksingh4435@gmail.com>
2025-10-28 11:53:32 +0530

    cors

--- trading/settings.py
+++ trading/settings.py
@@ -142,6 +142,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://cotp-demo.vercel.app/"
 ]
 
 CSRF_TRUSTED_ORIGINS = ['https://cotp.suprixsolution.in','http://localhost:8000']
